074a8513b92cde4b2ae9956a43c5c74c68aa83ba
Colby Taperts <colbytaperts@gmail.com>
2018-06-23 18:10:25 -0500

    refactor webpack

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -3,19 +3,15 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 
 const config = {
-  // inform webpack we are building a bundle for nodejs
-  // rather than for the browser
-  target: 'node',
-
   // tell webpack the root file of our
   // server app
-  entry: './src/index.js',
+  entry: './src/client/client.js',
 
   // tell webpack where to put the output file
   // that is generated
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'public')
   }
 };
 
