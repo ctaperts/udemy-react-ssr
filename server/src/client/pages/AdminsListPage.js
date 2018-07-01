898e8cecbe74400c57c172744a2ebf7e5bad8c94
Colby Taperts <colbytaperts@gmail.com>
2018-06-30 21:35:45 -0500

    better redirect for ssr

---
+++ server/src/client/pages/AdminsListPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchAdmins } from '../actions';
+import requireAuth from '../components/hocs/requireAuth';
+
+class AdminsList extends Component {
+  componentDidMount() {
+    this.props.fetchAdmins();
+  }
+
+  renderAdmins() {
+    return this.props.admins.map(admin => {
+      return <li key={admin.id}>{admin.name}</li>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        List of admins:
+        <ul>
+          {this.renderAdmins()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ admins }) {
+  return { admins };
+}
+
+export default {
+  loadData: ({ dispatch }) => dispatch(fetchAdmins()),
+  component: connect(mapStateToProps, { fetchAdmins })(requireAuth(AdminsList))
+};
