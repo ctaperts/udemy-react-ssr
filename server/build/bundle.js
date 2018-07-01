0ac5aeb04ea2588bc0407f364a35d43229bff5f6
Colby Taperts <colbytaperts@gmail.com>
2018-07-01 00:08:04 -0500

    add in helmet

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -542,6 +542,8 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactRedux = __webpack_require__(3);
 
+var _reactHelmet = __webpack_require__(28);
+
 var _actions = __webpack_require__(1);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -578,11 +580,26 @@ var UsersList = function (_Component) {
       });
     }
   }, {
+    key: 'head',
+    value: function head() {
+      return _react2.default.createElement(
+        _reactHelmet.Helmet,
+        null,
+        _react2.default.createElement(
+          'title',
+          null,
+          this.props.users.length + ' Users Loaded'
+        ),
+        _react2.default.createElement('meta', { property: 'og:title', content: 'Users App' })
+      );
+    }
+  }, {
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
         'div',
         null,
+        this.head(),
         'List of users:',
         _react2.default.createElement(
           'ul',
@@ -669,6 +686,8 @@ var _serializeJavascript = __webpack_require__(18);
 
 var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
 
+var _reactHelmet = __webpack_require__(28);
+
 var _Routes = __webpack_require__(4);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -690,7 +709,9 @@ exports.default = function (req, store, context) {
     )
   ));
 
-  return '\n  <html>\n    <head>\n      <!-- Compiled and minified CSS -->\n      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">\n    </head>\n    <body>\n      <div id="root">' + content + '</div>\n      <script>\n      window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '\n      </script>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
+  var helmet = _reactHelmet.Helmet.renderStatic();
+
+  return '\n  <html>\n    <head>\n      ' + helmet.title.toString() + '\n      ' + helmet.meta.toString() + '\n      <!-- Compiled and minified CSS -->\n      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">\n    </head>\n    <body>\n      <div id="root">' + content + '</div>\n      <script>\n      window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '\n      </script>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
 };
 
 /***/ }),
@@ -1021,6 +1042,12 @@ exports.default = function (ChildComponent) {
 
   return (0, _reactRedux.connect)(mapStateToProps)(RequireAuth);
 };
+
+/***/ }),
+/* 28 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-helmet");
 
 /***/ })
 /******/ ]);
